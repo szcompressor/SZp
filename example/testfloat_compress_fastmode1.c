d857b4636d33d6f9af36cab27ff944fa0427daf4
JiajunHuang1999 <57979997+JiajunHuang1999@users.noreply.github.com>
2025-06-25 10:49:24 -0500

    Update testfloat_compress_fastmode1.c

--- example/testfloat_compress_fastmode1.c
+++ example/testfloat_compress_fastmode1.c
@@ -59,8 +59,8 @@ int main(int argc, char *argv[])
 
   size_t outSize;
   cost_start();
-  unsigned char *bytes = szp_float_openmp_threadblock_randomaccess(data, &outSize, errBound, nbEle, blockSize);
-  // unsigned char *bytes = szp_float_openmp_threadblock(data, &outSize, errBound, nbEle, blockSize);
+  // unsigned char *bytes = szp_float_openmp_threadblock_randomaccess(data, &outSize, errBound, nbEle, blockSize);
+  unsigned char *bytes = szp_float_openmp_threadblock(data, &outSize, errBound, nbEle, blockSize);
   cost_end();
   printf("\ntimecost=%f, total fastmode1\n", totalCost);
   printf("compression size = %zu, CR = %f\n", outSize, 1.0f * nbEle * sizeof(float) / outSize);
